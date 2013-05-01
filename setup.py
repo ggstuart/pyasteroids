a1b9174a9fee4d7f7ccd36de0e713b05d874cc38
Graeme Stuart <ggstuart@gmail.com>
2013-05-01 23:09:36 +0100

    added some extra meta-data

--- setup.py
+++ setup.py
@@ -6,11 +6,24 @@ except ImportError:
     print 'You need to install pyGTK to use this'
     exit()
 
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
 setup(name='pyasteroids',
-    version='0.5',
+    version='0.5.1',
     description='Python asteroids game demonstrating pyagents library',
+    long_description=readme(),
     author='Graeme Stuart',
     author_email='ggstuart@gmail.com',
+    keywords='agent based asteroids'
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: GNU General Public License (GPL)'
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Games/Entertainment :: Arcade',
+        'Environment :: X11 Applications :: GTK',
+    ],
     url='https://github.com/ggstuart/pyasteroids.git',
     license='GPL',
     packages=[
